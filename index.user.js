c8b64c7e4ffc637d9a3e48ec3ab392f2ef403065
杨人瑞 <yangrenruiyrr@163.com>
2026-01-06 19:27:58 +0800

    Update index.user.js

--- index.user.js
+++ index.user.js
@@ -38,7 +38,7 @@
 // @grant        GM_getResourceText
 // @grant        GM_cookie
 // @grant        GM_listValues
-// @license      CC-BY-NC-ND-4.0\
+// @license      CC-BY-NC-ND-4.0
 // @require      https://cdn.jsdmirror.com/npm/sweetalert2@11.26.17/dist/sweetalert2.min.js
 // @require      https://cdn.jsdmirror.com/gh/highlightjs/cdn-release/build/highlight.min.js
 // @require      https://cdn.jsdmirror.com/npm/marked@4.0.0/marked.min.js
